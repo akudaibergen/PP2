171c9e91df6b54e51d893680b53c4885ddad9922
akudaibergen <47215344+akudaibergen@users.noreply.github.com>
2019-02-14 23:20:28 +0600

    Update Program.cs

--- Lab1/Task2/Program.cs
+++ Lab1/Task2/Program.cs
@@ -8,22 +8,22 @@ namespace Task2
 {
     class Student
     {
-        public string name;
-        public string ID;
-        public int year;
-        public Student(string name, string id, string year)
+        public string name; // creating public variable "name"
+        public string ID;// creating public variable "ID"
+        public int year;// creating public variable "year"
+        public Student(string name, string id, string year)// class that will get info of student
         {
-            this.name = name;
-            this.ID = id;
-            this.year = int.Parse(year);
+            this.name = name;// our public "name" is equal to the public variable
+            this.ID = id;// our public "id" is equal to the public variable
+            this.year = int.Parse(year);// our public "year" is equal to the public variable
         }
-        public void PrintStudent()
+        public void PrintStudent() // function that will print the variables
         {
-            Console.WriteLine(name);
-            Console.WriteLine(ID);
-            Console.WriteLine(year);
+            Console.WriteLine(name);// prints name
+            Console.WriteLine(ID);// prints ID
+            Console.WriteLine(year);// prints year
         }
-        public void IncreamentYear()
+        public void IncreamentYear()// increamenting method
         {
             this.year++;
             
@@ -34,11 +34,11 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Student student = new Student("Kudaibergen Adil", "18BD116446", "1");
-            student.PrintStudent();
-            student.IncreamentYear();
-            student.PrintStudent();
-             Console.ReadKey();
+            Student student = new Student("Kudaibergen Adil", "18BD116446", "1");// creating class student with 3 parametrs
+            student.PrintStudent();// urging the printing function
+            student.IncreamentYear();// urging increamenting method
+            student.PrintStudent();// urging the printing function
+             
         }
        
     }
